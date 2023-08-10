3770385d1460cf3152d82eb6d67f91e90e286e3a
s22004 <s22004@std.it-college.ac.jp>
2023-08-10 10:02:16 +0900

    setting layout

--- app/src/main/java/jp/ac/it_college/std/s22004/gteampbl/SettingActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s22004/gteampbl/SettingActivity.kt
@@ -3,7 +3,7 @@ package jp.ac.it_college.std.s22004.gteampbl
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
